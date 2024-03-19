c2d9b8c23478f3846019b04fb83dca360a00ea22
Kinza Qureshi <ikinzaqureshi@gmail.com>
2024-03-19 22:06:35 +0000

    Adding cloudinary as the default storage

--- wellread/settings.py
+++ wellread/settings.py
@@ -177,6 +177,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+
+# Cloudinary Settings
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUIDNARY_URL = os.environ.get('CLOUDINARY_URL')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
