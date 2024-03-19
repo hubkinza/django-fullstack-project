56ea901f19fd0d6f905daa38c592fe28c926e924
Kinza Qureshi <ikinzaqureshi@gmail.com>
2024-03-19 21:58:33 +0000

    Added genre and language to books model

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book,Genre,Language
+
+@admin.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "image",
+        "genre",
+        "language",   
+       
+    )
+    list_filter = ('title', 'author',)
+        
--- books/models.py
+++ books/models.py
@@ -4,6 +4,20 @@ from django_resized import ResizedImageField
 # import user model 
 from django.contrib.auth.models import User
 
+#Adding class for genre 
+class Genre(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
 
 # A model to create and manage books
 class Book(models.Model):
@@ -18,6 +32,11 @@ class Book(models.Model):
         blank=False,
         null=False,
     )
-
+    isbn_number = models.CharField(max_length=13, null=True, blank=True)
+    genre = models.ForeignKey(Genre, null=True, blank=False, on_delete=models.CASCADE)
+    language = models.ForeignKey(
+        Language, null=True, blank=False, on_delete=models.CASCADE
+    )
+    description = models.TextField(max_length=300, null=True, blank=False,)
     def __str__(self):
         return self.title
