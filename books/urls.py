21e32ebe178603f61114eb8eba91de56f4607b12
Kinza Qureshi <ikinzaqureshi@gmail.com>
2024-03-20 22:44:05 +0000

    Add url for delete

--- books/urls.py
+++ books/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("AddBook/", views.AddBook, name="AddBook"),
     path("books/", views.Books.as_view(), name="books"),
     path("<slug:pk>/", views.BookDetail.as_view(), name="book_details"),
-     path("edit/<int:id>/", views.edit_book, name="edit_book"),
+    path("edit/<int:id>/", views.edit_book, name="edit_book"),
+      path("delete/<slug:id>/", views.delete_book, name="delete_book"),
 ]
 
