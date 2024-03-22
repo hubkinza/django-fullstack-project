2dc5b483b2597336ef537114a0d097b7a76c7f78
Kinza Qureshi <ikinzaqureshi@gmail.com>
2024-03-22 11:58:52 +0000

    remove extra model added to test inital setup

--- events/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from cloudinary.models import CloudinaryField
-
-class Events(models.Model):
-    title = models.CharField(max_length=100)
-    image = CloudinaryField('image', default='placeholder')
